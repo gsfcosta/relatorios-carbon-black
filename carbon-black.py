a683856c5171c60b1f78638a5990c99b473bc302
Gabriel Silva <42187636+gsfcosta@users.noreply.github.com>
2022-08-31 21:38:54 -0300

    Update carbon-black.py

--- carbon-black.py
+++ carbon-black.py
@@ -9,7 +9,6 @@ db_hostname         = (sys.argv[2])
 db_username         = (sys.argv[3])
 db_password         = (sys.argv[4])
 db_name             = "carbonblack_stg"
-db_driver           = '{ODBC Driver 17 for SQL Server}'
 
 # CB vars
 cb_api_id           = (sys.argv[5])
@@ -18,12 +17,13 @@ cb_tenant           = (sys.argv[7])
 cb_url              = "https://defense-prod05.conferdeploy.net"
 
 try:
-    conexao = pyodbc.connect('DRIVER='+db_driver+';SERVER=tcp:'+db_hostname+';PORT=1433;DATABASE='+db_name+';UID='+db_username+';PWD='+ db_password)
+    conexao = pyodbc.connect("DRIVER={ODBC Driver 17 for SQL Server};SERVER=%s;DATABASE=%s;UID=%s;PWD=%s" % (db_hostname, db_name, db_username, db_password))
     cursor = conexao.cursor()
     headers = {
     'content-type': "application/json",
     'X-AUTH-TOKEN': cb_api_secret_key + "/" + cb_api_id,
     }
     funcao(headers, cursor, cb_tenant, cb_url)
-except:
-    print("Falha na conexão com o banco de dados")
+except BaseException as e:
+    print("Falha na conexão com o banco de dados. Erro: " + str(e))
+
